a577a02c00ddafabe8091cb02db07fc4fc48e7d5
Magda <magdagozdalik28@gmail.com>
2021-11-11 17:16:49 +0000

    Clean up in scss and js files

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,7 @@
 import firebase from "firebase/app";
 import "firebase/analytics";
 import "firebase/firestore";
+import "firebase/auth";
 
 const firebaseConfig = {
     // wasze ustawienia
@@ -12,9 +13,11 @@ const firebaseConfig = {
     appId: "1:415324695182:web:248afb658b15004243b7df"
 };
 
-firebase.initializeApp(firebaseConfig);
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export {auth};
+export default db;
+
 firebase.analytics();
-
-const db = firebase.firestore();
-
-export { db };
