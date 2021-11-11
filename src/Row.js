e732446b5f331320b5d1556661fb1901ac903d70
Magda <magdagozdalik28@gmail.com>
2021-11-11 17:22:00 +0000

    Updated Row.js

--- src/Row.js
+++ src/Row.js
@@ -1,19 +1,34 @@
-import {useState} from 'react';
-import "./Row.scss"
+import React, {useState} from 'react';
+import db from "./firebase";
+
 import {API_KEY} from "./api/constants";
 import MovieInfo from "./MovieInfo";
+import "./Row.scss";
+import "./genericStyles/icon.scss";
+import firebase from "firebase";
 
-const Row = ({title, movies, rowTitle}) => {
+const Row = ({title, movies, rowTitle, user}) => {
 
     const [visibleId, setVisibleId] = useState("");
 
+    const handleAddToList = (clickedId, movie) => {
+        if (clickedId == visibleId) {
+            db.collection("users").doc(user.uid).update({
+                favourites: firebase.firestore.FieldValue.arrayUnion(movie)
+            })
+                .then(() => {
+                    console.log("added")
+                })
+        }
+    };
+
     return (
-        <section className="rows">
+        <section className="rows" style={{position: "relative"}}>
             <div className="row">
                 <h2 className="row__title">
                     {title}
                 </h2>
-                <div className="row__posters">
+                <div className="row__posters ">
                     {movies.map((movie) => {
                         const id = movie.id;
                         const className = id == visibleId ? "visible" : "hidden";
@@ -23,14 +38,14 @@ const Row = ({title, movies, rowTitle}) => {
                                     onMouseEnter={(e) => setVisibleId(e.currentTarget.id)}
                                     onMouseLeave={() => setVisibleId("")}>
 
-                            <img className={`row__posterImg ${rowTitle === "netflixOriginals" && "row__posterLarge"}`}
-                                 src={`https://image.tmdb.org/t/p/original${rowTitle === "netflixOriginals" ? movie.poster_path : movie.backdrop_path}?=${API_KEY}`}
+                            <img className={`row__posterImg ${rowTitle === "netflixOriginals" && "row__posterLarge"}
+                    ${id == visibleId && "imageHovered"}`}
+                                 src={`https://image.tmdb.org/t/p/original${rowTitle === "netflixOriginals" ? movie.poster_path : movie.backdrop_path}?=${API_KEY}}`}
                                  alt={movie.title || movie.original_title || movie.name}/>
-                            <MovieInfo movie={movie} className={className} id={id}/>
+                            <MovieInfo movie={movie} className={className} id={id} handleAddToList={handleAddToList}/>
                         </div>
                     })}
                 </div>
-
             </div>
         </section>
     );
