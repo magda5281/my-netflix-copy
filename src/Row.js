5af7c6f90ea482ef685ed912309545a1589e01f6
Magdalena Gozdalik <magdagozdalik28@gmail.com>
2025-01-03 09:51:58 +0000

    Fixed eslint warnings

--- src/Row.js
+++ src/Row.js
@@ -1,53 +1,70 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
 import db from "./firebase";
 
-import {API_KEY} from "./api/constants";
+import { API_KEY } from "./api/constants";
 import MovieInfo from "./MovieInfo";
 import "./Row.scss";
 import "./genericStyles/icon.scss";
 import firebase from "firebase";
 
-const Row = ({title, movies, rowTitle, user}) => {
-    const [visibleId, setVisibleId] = useState("");
-    const showPar = true;
-    const handleAddToList = (clickedId, movie) => {
-        if (clickedId == visibleId) {
-            db.collection("users").doc(user.uid).update({
-                favourites: firebase.firestore.FieldValue.arrayUnion(movie)
-            })
-                .then(() => {
-                    console.log("added")
-                })
-        }
-    };
+const Row = ({ title, movies, rowTitle, user }) => {
+  const [visibleId, setVisibleId] = useState("");
+  const showPar = true;
+  const handleAddToList = (clickedId, movie) => {
+    if (clickedId === visibleId) {
+      db.collection("users")
+        .doc(user.uid)
+        .update({
+          favourites: firebase.firestore.FieldValue.arrayUnion(movie),
+        })
+        .then(() => {
+          console.log("added");
+        });
+    }
+  };
 
-    return (
-        <section className="rows" style={{position: "relative"}}>
-            <div className="row">
-                <h2 className="row__title">
-                    {title}
-                </h2>
-                <div className="row__posters ">
-                    {movies.map((movie) => {
-                        const id = movie.id;
-                        const className = id == visibleId ? "visible" : "hidden";
-                        return <div key={movie.id}
-                                    className="row__poster"
-                                    id={movie.id}
-                                    onMouseEnter={(e) => setVisibleId(e.currentTarget.id)}
-                                    onMouseLeave={() => setVisibleId("")}>
-
-                            <img className={`row__posterImg ${rowTitle === "netflixOriginals" && "row__posterLarge"}
-                    ${id == visibleId && "imageHovered"}`}
-                                 src={`https://image.tmdb.org/t/p/original${rowTitle === "netflixOriginals" ? movie.poster_path : movie.backdrop_path}?=${API_KEY}}`}
-                                 alt={movie.title || movie.original_title || movie.name}/>
-                            <MovieInfo movie={movie} className={className} id={id} handleAddToList={handleAddToList} showPar={showPar}/>
-                        </div>
-                    })}
-                </div>
-            </div>
-        </section>
-    );
+  return (
+    <section className="rows" style={{ position: "relative" }}>
+      <div className="row">
+        <h2 className="row__title">{title}</h2>
+        <div className="row__posters ">
+          {movies.map((movie) => {
+            const id = movie.id;
+            const className = id === visibleId ? "visible" : "hidden";
+            return (
+              <div
+                key={movie.id}
+                className="row__poster"
+                id={movie.id}
+                onMouseEnter={(e) => setVisibleId(e.currentTarget.id)}
+                onMouseLeave={() => setVisibleId("")}
+              >
+                <img
+                  className={`row__posterImg ${
+                    rowTitle === "netflixOriginals" && "row__posterLarge"
+                  }
+                    ${id === visibleId && "imageHovered"}`}
+                  src={`https://image.tmdb.org/t/p/original${
+                    rowTitle === "netflixOriginals"
+                      ? movie.poster_path
+                      : movie.backdrop_path
+                  }?=${API_KEY}}`}
+                  alt={movie.title || movie.original_title || movie.name}
+                />
+                <MovieInfo
+                  movie={movie}
+                  className={className}
+                  id={id}
+                  handleAddToList={handleAddToList}
+                  showPar={showPar}
+                />
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Row;
--- src/screens/MyListScreen.js
+++ src/screens/MyListScreen.js
@@ -1,41 +1,51 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
 import Nav from "../Nav";
 import "./MyListScreen.scss";
-import "../Row.scss"
-import {API_KEY} from "../api/constants";
+import "../Row.scss";
+import { API_KEY } from "../api/constants";
 import MovieInfo from "../MovieInfo";
 
-const MyListScreen = ({myList}) => {
-
-    const [visibleId, setVisibleId] = useState("");
-    const showPar = false;
-    return (
-        <>
-            <Nav/>
-            <section className="myListScreen row">
-                <h1>My List</h1>
-                <div className="myListScreen__wrapper row__posters">
-                    {myList.map((item) => {
-                        console.log(item)
-                        const id = item.id;
-                        const className = id == visibleId ? "visible" : "hidden";
-                        return <div key={item.id}
-                                    className=" myListScreen__poster row__poster"
-                                    id={item.id}
-                                    onMouseEnter={(e) => setVisibleId(e.currentTarget.id)}
-                                    onMouseLeave={() => setVisibleId("")}>
-
-                            <img className={`row__posterImg ${id == visibleId && "imageHovered"}`}
-                                 src={`https://image.tmdb.org/t/p/original${item.backdrop_path}?=${API_KEY}}`}
-                                 alt={item.title || item.original_title || item.name}/>
-                            <MovieInfo movie={item} className={className} id={id} showPar={showPar}/>
-                        </div>
-                    })}
-                </div>
-            </section>
-        </>
-
-    )
-}
+const MyListScreen = ({ myList }) => {
+  const [visibleId, setVisibleId] = useState("");
+  const showPar = false;
+  return (
+    <>
+      <Nav />
+      <section className="myListScreen row">
+        <h1>My List</h1>
+        <div className="myListScreen__wrapper row__posters">
+          {myList.map((item) => {
+            console.log(item);
+            const id = item.id;
+            const className = id === visibleId ? "visible" : "hidden";
+            return (
+              <div
+                key={item.id}
+                className=" myListScreen__poster row__poster"
+                id={item.id}
+                onMouseEnter={(e) => setVisibleId(e.currentTarget.id)}
+                onMouseLeave={() => setVisibleId("")}
+              >
+                <img
+                  className={`row__posterImg ${
+                    id === visibleId && "imageHovered"
+                  }`}
+                  src={`https://image.tmdb.org/t/p/original${item.backdrop_path}?=${API_KEY}}`}
+                  alt={item.title || item.original_title || item.name}
+                />
+                <MovieInfo
+                  movie={item}
+                  className={className}
+                  id={id}
+                  showPar={showPar}
+                />
+              </div>
+            );
+          })}
+        </div>
+      </section>
+    </>
+  );
+};
 
 export default MyListScreen;
