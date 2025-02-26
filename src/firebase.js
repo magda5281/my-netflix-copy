66bbc49a2405d01b8a3a11ac769e6779a2af9441
Magdalena Gozdalik <magdagozdalik28@gmail.com>
2025-02-26 16:19:50 +0000

    Update

--- src/firebase.js
+++ src/firebase.js
@@ -1,23 +1,23 @@
-import firebase from "firebase/app";
-import "firebase/analytics";
-import "firebase/firestore";
-import "firebase/auth";
+import firebase from 'firebase/app';
+import 'firebase/analytics';
+import 'firebase/firestore';
+import 'firebase/auth';
 
 const firebaseConfig = {
-    // wasze ustawienia
-    apiKey: "AIzaSyA-tzif3hipCgN_OsGje0bkv9L72BiPYfM",
-    authDomain: "my-netflix-copy.firebaseapp.com",
-    projectId: "my-netflix-copy",
-    storageBucket: "my-netflix-copy.appspot.com",
-    messagingSenderId: "415324695182",
-    appId: "1:415324695182:web:248afb658b15004243b7df"
+  // wasze ustawienia
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 const db = firebaseApp.firestore();
 const auth = firebase.auth();
 
-export {auth};
+export { auth };
 export default db;
 
 firebase.analytics();
