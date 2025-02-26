794f1dbf9137f63ed73bd71a93458351270e6e84
Magdalena Gozdalik <magdagozdalik28@gmail.com>
2025-02-26 21:34:31 +0000

    Update logo

--- src/Nav.js
+++ src/Nav.js
@@ -1,45 +1,49 @@
-import React, {useEffect, useState} from 'react';
-import {useNavigate} from "react-router-dom";
-import "./Nav.scss";
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import './Nav.scss';
+import movieBoardIcon from '../src/assets/icons8-movie-projector.png';
 
 const Nav = () => {
-    const [showSolidNav, handleShowSolidNav] = useState(false);
-    const navigate = useNavigate();
+  const [showSolidNav, handleShowSolidNav] = useState(false);
+  const navigate = useNavigate();
 
-    useEffect(() => {
-        window.addEventListener("scroll", () => {
-            if (window.scrollY > 100) {
-                handleShowSolidNav(true);
-            } else handleShowSolidNav(false);
-        });
-        return window.removeEventListener("scroll", handleShowSolidNav(false));
-    }, []);
-    return (
-        <nav className={`nav ${showSolidNav ? "nav__solid" : "nav__faded"}`}>
-            <div className="nav__logoContainer">
-                <img src="https://upload.wikimedia.org/wikipedia/commons/7/7a/Logonetflix.png"
-                     alt="Netflix logo "
-                     className="nav__logo"
-                     onClick={() => navigate("/")}/>
-            </div>
-            <ul className="nav__primary">
-                <li className="nav__tab">Main page</li>
-                <li className="nav__tab">Series and Tv</li>
-                <li className="nav__tab">Movies</li>
-                <li className="nav__tab">New</li>
-                <li className="nav__tab"
-                    onClick={() => navigate("/myList")}>
-                    My list
-                </li>
-            </ul>
-            <div>
-                <img src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"
-                     alt="Netflix logo "
-                     className="nav__avatar"
-                     onClick={() => navigate("/profile")}/>
-            </div>
-        </nav>
-    );
+  useEffect(() => {
+    window.addEventListener('scroll', () => {
+      if (window.scrollY > 100) {
+        handleShowSolidNav(true);
+      } else handleShowSolidNav(false);
+    });
+    return window.removeEventListener('scroll', handleShowSolidNav(false));
+  }, []);
+  return (
+    <nav className={`nav ${showSolidNav ? 'nav__solid' : 'nav__faded'}`}>
+      <div className='nav__logoContainer'>
+        <img
+          src={movieBoardIcon}
+          alt='Movie app logo '
+          className='nav__logo'
+          onClick={() => navigate('/')}
+        />
+      </div>
+      <ul className='nav__primary'>
+        <li className='nav__tab'>Main page</li>
+        <li className='nav__tab'>Series and Tv</li>
+        <li className='nav__tab'>Movies</li>
+        <li className='nav__tab'>New</li>
+        <li className='nav__tab' onClick={() => navigate('/myList')}>
+          My list
+        </li>
+      </ul>
+      <div>
+        <img
+          src='https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png'
+          alt=' avatar '
+          className='nav__avatar'
+          onClick={() => navigate('/profile')}
+        />
+      </div>
+    </nav>
+  );
 };
 
 export default Nav;
